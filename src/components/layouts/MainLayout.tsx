ebf1b9cbf79db763b253ed18ceaa87c2d97a19b2
xSkkarf <mostafa.ayyad.117@gmail.com>
2025-04-20 16:03:58 +0200

    Initial setup

---
+++ src/components/layouts/MainLayout.tsx
@@ -0,0 +1,12 @@
+
+const MainLayout = () => {
+  return (
+    <div className="flex h-screen flex-col">
+        <div className="mt-30">
+            <h1>hello</h1>
+        </div>
+    </div>
+  )
+}
+
+export default MainLayout
---
+++ src/main.tsx
@@ -0,0 +1,5 @@
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import MainLayout from "./components/layouts/MainLayout";
+
+createRoot(document.getElementById("root")!).render(<MainLayout />);
